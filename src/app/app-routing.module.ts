e79f7e2e7d3999223a58103c380ed6ba55c67d5d
vipnua <giangvipbg2002@gmail.com>
2022-10-16 21:43:10 +0700

    PA-2 | Update giao dien admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { AdminMainComponent } from './admin/layouts/admin-main/admin-main.component';
+const routes: Routes = [
+  {path:'admin',component:AdminMainComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminHeaderComponent } from './admin/layouts/admin-header/admin-header.component';
+import { AdminFooterComponent } from './admin/layouts/admin-footer/admin-footer.component';
+import { AdminSiderBarComponent } from './admin/layouts/admin-sider-bar/admin-sider-bar.component';
+import { AdminMainComponent } from './admin/layouts/admin-main/admin-main.component';
+import { ContentComponent } from './admin/components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminHeaderComponent,
+    AdminFooterComponent,
+    AdminSiderBarComponent,
+    AdminMainComponent,
+    ContentComponent,
   ],
   imports: [
     BrowserModule,
